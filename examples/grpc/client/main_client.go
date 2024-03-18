619b861feb74ce46e122bc9e43bd2829ee1063b5
huzejun <961639130@qq.com>
2024-03-18 19:47:34 +0800

    lark-Golang千万级IM服务端项目 第一个grpc服务 2024-3-18 19:47:12

---
+++ examples/grpc/client/main_client.go
@@ -0,0 +1,34 @@
+// @Author huzejun 2024/3/18 0:45:00
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"lark/examples/pb_auth"
+)
+
+func main() {
+	req := &pb_auth.SignUpReq{
+		Nickname: "lark",
+	}
+	conn, err := grpc.Dial("127.0.0.1:8001", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer conn.Close()
+	client := pb_auth.NewAuthClient(conn)
+	var resp *pb_auth.SignUpResp
+	resp, err = client.SignUp(context.Background(), req)
+	if err != nil {
+
+		fmt.Println(err.Error())
+		return
+	}
+	if resp == nil {
+		return
+	}
+	fmt.Println(resp.Msg)
+}
