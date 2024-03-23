12be9b6156bff13f28619364c5b3aca2b13934be
huzejun <961639130@qq.com>
2024-03-24 03:18:56 +0800

    lark-Golang千万级IM服务端项目 lark-工具包导入 2024-3-24 03:17:57

---
+++ pkg/common/xredis/lock.go
@@ -0,0 +1,7 @@
+// @Author huzejun 2024/3/23 17:45:00
+package xredis
+
+//func NewMutex(key string, options ...redisync.Option) *redisync.Mutex {
+//	key = RealKey(key)
+//	return cli.RedsSync.NewNutex(key, options...)
+//}
---
+++ pkg/proto/pb_auth/resp.go
@@ -0,0 +1,36 @@
+package pb_auth
+
+func (r *SignUpResp) Set(code int32, msg string) {
+	r.Code = code
+	r.Msg = msg
+}
+
+func (r *SignInResp) Set(code int32, msg string) {
+	r.Code = code
+	r.Msg = msg
+}
+
+func (r *SignOutResp) Set(code int32, msg string) {
+	r.Code = code
+	r.Msg = msg
+}
+
+func (r *RefreshTokenResp) Set(code int32, msg string) {
+	r.Code = code
+	r.Msg = msg
+}
+
+func (r *GithubOAuth2CallbackResp) Set(code int32, msg string) {
+	r.Code = code
+	r.Msg = msg
+}
+
+func (r *GoogleOAuth2CallbackResp) Set(code int32, msg string) {
+	r.Code = code
+	r.Msg = msg
+}
+
+func (r *AuthUserInfo) Set(code int32, msg string) {
+	r.Code = code
+	r.Msg = msg
+}
